252fe4e7f9cbf6cb015c19f3c1fab12520b44a12
Samuel Skoog <samule.skoog@elev.ga.ntig.se>
2023-08-31 10:32:33 +0200

    uiiului

---
+++ LuckText/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Vad är du för något");
+
+string skapelse = Console.ReadLine();
+
+Console.WriteLine("Fan va coolt, men frågan är... gillar du män?");
+
+string sexualitet = Console.ReadLine();
+
+Console.WriteLine("Är du helt säker på det?");
+
+string hmmm = Console.ReadLine();
+
+Console.WriteLine($"Okej, så du är en {skapelse} och du gillar män? {sexualitet}. Är du helt säker? {hmmm}. Men kan inte vara 100% förs man har testat.");
+
+Console.ReadLine();
